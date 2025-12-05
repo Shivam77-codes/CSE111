384a5e4cfd5acfc35fa9b9d7424fc5119fedfff4
Shivam Sharma <shivamsharma41081@gmail.com>
2025-12-05 12:14:36 +0530

    Add files via upload

---
+++ Bitwise operation.py
@@ -0,0 +1,20 @@
+a=5
+b=6
+print(a&b) #This is AND(&) operator 
+
+a=54
+b=89
+print(a|b)#This is OR(|) operator
+
+a=78
+b=56
+print(a^b) #This is XOR(^) operator
+
+a=90
+b=89
+print(a<<1) #This is Left shift operation and this is by 1 decimal
+print(b>>2)#This is Right shift operation by 2 decimal
+
+
+a=9
+print(~a) #This is not function means the bitnary number of a is being fliped then the assign to it is printed which is equal to -(x+1)
